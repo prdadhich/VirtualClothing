aaf5dda30ae5442f5581c5f8e3853d8739835ae3
pratyush dadhich <prdadhich@rediffmail.com>
2022-03-30 17:42:35 +0530

    first commit

---
+++ Source/VirtualClothing/VirtualClothingGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "VirtualClothingGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VIRTUALCLOTHING_API AVirtualClothingGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
